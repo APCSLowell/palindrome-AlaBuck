ad1209c1fc6a1a1ff6b22d39e5d692fc0733a9e0
AlaBuck <Albuckingham@s.sfusd.edu>
2025-01-28 10:23:09 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,43 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String wordlow = noCapitals(word);
+  String wordxspace = noSpaces(wordlow);
+  if(wordxspace.equals(reverse(wordxspace))){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
-    return sNew;
+    String revWord = "";
+    int leng = sWord.length();
+    for(int i=sWord.length(); i>0; i--){
+    revWord+=sWord.substring(i-1,i);
+  }
+  return revWord;
 }
+public String noCapitals(String sWord){
+  String lower = sWord.toLowerCase();
+  return lower;
+}
+public String noSpaces(String sWord){
+  String nospaces = "";
+  for(int i=0; i<sWord.length(); i++){
+    if(!sWord.substring(i,i+1).equals(" ")){
+      nospaces+=sWord.substring(i,i+1);
+    }
+  }
+  return nospaces;
+}
+public String onlyLetters(String sString){
+  String onlyLets = new String();
+  for(int i=0; i<sString.length(); i++){
+    if(Character.isLetter(sString.charAt(i))){
+      onlyLets+=sString.substring(i,i+1);
+    }
+  }
+  return onlyLets;
 }
